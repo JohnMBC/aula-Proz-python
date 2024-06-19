083a0cf6580576e48aab5863ae239904259d6c85
Johnny Michael Borges Carvalho <101338427+JohnMBC@users.noreply.github.com>
2024-06-19 23:46:42 +0000

    calculadora basica

---
+++ exercicio.py
@@ -0,0 +1,10 @@
+# Crie um programa que receba dois numero do usuario e exiba a soma, subtração, multiplicação e divisão dos dois numeros
+
+numero1= int(input("Digite um numero: "))
+numero2= int(input("Digite um numero: "))
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+
+#                  
