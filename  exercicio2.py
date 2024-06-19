e98c82c865cff641515106822b21c82dc36e4ce0
Johnny Michael Borges Carvalho <101338427+JohnMBC@users.noreply.github.com>
2024-06-19 23:58:48 +0000

    calculo de media

---
+++  exercicio2.py
@@ -0,0 +1,6 @@
+#Escreva um programa que solicite ao usuário para inserir três numeros em seguida, exiba a média desses números
+numero1= int(input("Digite um numero: "))
+numero2= int(input("Digite um numero: "))
+numero3= int(input("Digite um numero: "))
+media= (int(numero1 + numero2 + numero3 ) / 3)
+print (media)
