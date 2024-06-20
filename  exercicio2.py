69657a62040d54a7a676674a1fbef8958c546785
Johnny Michael Borges Carvalho <101338427+JohnMBC@users.noreply.github.com>
2024-06-20 00:10:56 +0000

    conversao de metros em centimetros

---  exercicio2.py
+++  exercicio2.py
@@ -2,5 +2,5 @@
 numero1= int(input("Digite um numero: "))
 numero2= int(input("Digite um numero: "))
 numero3= int(input("Digite um numero: "))
-media= (int(numero1 + numero2 + numero3 ) / 3)
+media= ((numero1 + numero2 + numero3 ) / 3)
 print (media)
---
+++ exercicio3.py
@@ -0,0 +1,6 @@
+#Escreva um programa que solicite ao usuário para inserir uma medida em metros e,
+#em seguida exiba  a medida convertida em centímetros
+
+metros= int(input("Digite um numero: "))
+convertido = ((metros * 100 ))
+print(convertido)
